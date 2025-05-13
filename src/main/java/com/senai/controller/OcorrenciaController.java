4fda9dfc56011b78217dc71edc308fff0c47d775
RafaelGibertoni <rafa08gibertoni@gmail.com>
2025-05-13 11:05:56 -0300

    ocorrencia

---
+++ src/main/java/com/senai/controller/OcorrenciaController.java
@@ -0,0 +1,40 @@
+package com.senai.controller;
+
+import com.senai.model.Ocorrencia;
+import com.senai.model.OcorrenciaDAO;
+
+import java.util.List;
+
+
+public class OcorrenciaController {
+    OcorrenciaDAO ocorrenciaDAO = new OcorrenciaDAO();
+
+    public List<Ocorrencia> listarOcorrencias(){
+        return (List<Ocorrencia>) ocorrenciaDAO.listar();
+    }
+
+    public boolean cadastrarOperador(Ocorrencia ocorrencia){
+        if (ocorrencia != null){
+            ocorrenciaDAO.salvar(ocorrencia);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean atualizarOcorrencias(Ocorrencia ocorrencia) {
+        if (ocorrencia != null) {
+            ocorrenciaDAO.atualizar(ocorrencia);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deletarOcorrencias(int id) {
+        if (id >= 0) {
+            ocorrenciaDAO.deletar(id);
+            return true;
+        }
+        return false;
+    }
+}
+
