7dc44dffed3e521a97a479b5007ee740aa31de66
Therciane <vasconcelostherciane@gmail.com>
2025-05-20 11:11:53 -0300

    Committ dia 20/05

--- src/main/java/com/senai/model/UnidadeCurricular.java
+++ src/main/java/com/senai/model/UnidadeCurricular.java
@@ -1,4 +1,45 @@
 package com.senai.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnidadeCurricular {
+    private String disciplina;
+    private double cargaHoraria;
+    private String subTurma;
+    private List<UnidadeCurricular> unidades;
+
+    public UnidadeCurricular(String disciplina, double cargaHoraria, String subTurma) {
+        this.disciplina = disciplina;
+        this.cargaHoraria = cargaHoraria;
+        this.subTurma = subTurma;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public double getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(double cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getSubTurma() {
+        return subTurma;
+    }
+
+    public void setSubTurma(String subTurma) {
+        this.subTurma = subTurma;
+    }
+
+    public List<UnidadeCurricular> listarUnidades() {
+        return unidades;
+    }
 }
