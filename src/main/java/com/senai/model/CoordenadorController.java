8da3fdc14d2e74256b5edcd907192e9fa36d3463
Pietra Rocha <pietrarocha72@gmail.com>
2025-05-21 09:26:14 -0300

    Código Coordenador atualizado 21/05/25

--- src/main/java/com/senai/model/CoordenadorController.java
+++ src/main/java/com/senai/model/CoordenadorController.java
@@ -1,4 +1,29 @@
 package com.senai.model;
+import java.util.List;
 
 public class CoordenadorController {
-}
+
+        private CoordenadorDAO dao = new CoordenadorDAO();
+
+        public void adicionarCoordenador(int id, String nome, String email) {
+            Coordenador coordenador = new Coordenador(id, nome, email, "padrao123");
+            dao.criar(coordenador);
+        }
+
+        public List<Coordenador> listarCoordenadores() {
+            return dao.listarTodos();
+        }
+
+        public Coordenador buscarCoordenador(int id) {
+            return dao.buscarPorId(id);
+        }
+
+        public void atualizarCoordenador(int id, String nome, String email) {
+            Coordenador coordenador = new Coordenador(id, nome, email, "padrao123");
+            dao.atualizar(coordenador);
+        }
+
+        public void deletarCoordenador(int id) {
+            dao.deletar(id);
+        }
+    }
