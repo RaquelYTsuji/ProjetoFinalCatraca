d0afb7a289a32055608af7d93f6fd2f9db6e321b
therciane <vasconcelostherciane@gmail.com>
2025-05-28 10:19:43 -0300

    .

--- src/main/java/com/senai/view/Main.java
+++ src/main/java/com/senai/view/Main.java
@@ -1,7 +1,9 @@
 package com.senai.view;
 
+import com.senai.model.Professor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello Word!");
     }
 }
